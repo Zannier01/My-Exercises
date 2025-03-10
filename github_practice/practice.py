2bdec41042f15cf14935179d8b58c611f0a7bb08
Rafail <rafail.donchev1@gmail.com>
2025-03-10 14:46:01 +0200

    New Function in new Branch

--- github_practice/practice.py
+++ github_practice/practice.py
@@ -1,6 +1,11 @@
+import random
 def practice(number):
     for i in range(number+1):
         print(i)
 
+# New Function to test commiting to new branch
+def random_function():
+    num1 = random.randint(10,10000)
+    print(num1)
 
-practice(9999)
+random_function()
