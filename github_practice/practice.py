76562d7b077edd5f6c37576f44726ff0f9923867
Simeon Olev <simeon.ludaka@gmail.com>
2025-03-12 17:56:52 +0200

    test

--- github_practice/practice.py
+++ github_practice/practice.py
@@ -1,11 +1,23 @@
+import random
 def practice(number):
     for i in range(number+1):
         print(i)
 
+# New Function to test commiting to new branch
+def random_function():
+    num1 = random.randint(10,10000)
+    print(num1)
 
 def add_numbers(a, b):
     return a + b
 
+
+def user_name():
+    name = input("Please state your name: ")
+    print(name)
+
 practice(9999)
 result = add_numbers(3, 5)
 print("Sum:", result)
+random_function()
+user_name()
